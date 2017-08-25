32e67e1c661768ff7bb9e1c072311a991d39fdf8
RJ-Amy1538 <31036053+RJ-Amy1538@users.noreply.github.com>
2017-08-25 21:58:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,33 @@ button.onclick = function () {
     if(request.readyState === XMLHttpRequest.DONE) {
         //take some action
         if(request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+     //not done yet
+  };
+  //make a request
+  request.open('GET', "http://rockstar1538.imad.hasura-app.io/counter", true);
+  request.send(null);
+};
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    //make a request to server and send the name
+    // create a request object
+  var request = new XMLHttpRequest();
+  
+  
+  //capture the response and store it in a variable
+  request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        //take some action
+        if(request.status === 200) {
          //capture a list of names and render it as a list
   var names = request.responseText;
   names = JSON.parse(names);
@@ -27,14 +54,5 @@ button.onclick = function () {
   //make a request
   request.open('GET', "http://rockstar1538.imad.hasura-app.io/submit-name?name=" + name, true);
   request.send(null);
-};
-
-//submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function () {
-    //make a request to server and send the name
-  
   
 };
