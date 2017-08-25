ab222599e7c30a9945d5262a8b8345970550446d
RJ-Amy1538 <31036053+RJ-Amy1538@users.noreply.github.com>
2017-08-25 18:32:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get('/', function (req, res) {
   var counter = 0;
 app.get('/counter', function(req, res) {
     counter = counter + 1;
-    res.semd(counter.toString());
+    res.send(counter.toString());
 });
 
 app.get('/:articleName', function (req, res) {
