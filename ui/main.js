3ccbb3f2a21cff17ae7f82be23e12ac66de20382
RJ-Amy1538 <31036053+RJ-Amy1538@users.noreply.github.com>
2017-08-25 20:57:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,4 +21,22 @@ button.onclick = function () {
   //make a request
   request.open('GET', "http://rockstar1538.imad.hasura-app.io/counter", true);
   request.send(null);
+
+
+//submit name
+var nameInput =document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function () {
+    //make a request to server and send the name
+  
+    //capture a list of names and frender it as a list
+  var names = ['name1','namw2','name3'];
+  var list = '';
+  for(var i=0;i<names.length;i++) {
+      list += '<li'> + names[i] + '</li>';
+  }
+  var ul = document.getElementById("namelist");
+  ul.innerHTML = list;
+  
 };
