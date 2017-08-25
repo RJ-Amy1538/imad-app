1fd1ee2b8cbe13d9ebd324806fa99111c9b5a91d
RJ-Amy1538 <31036053+RJ-Amy1538@users.noreply.github.com>
2017-08-25 21:07:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ submit.onclick = function () {
     //make a request to server and send the name
   
     //capture a list of names and frender it as a list
-  var names = ['name1','namw2','name3', 'name4'];
+  var names = ['name1','name2','name3', 'name4'];
   var list = '';
   for(var i=0;i<names.length;i++) {
       list += '<li'> + names[i] + '</li>';
