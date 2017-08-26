51d6a40766738127cbd8e5a2016dcc7e19b01975
RJ-Amy1538 <31036053+RJ-Amy1538@users.noreply.github.com>
2017-08-26 18:12:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ var articles = {
             ${heading}
         </h3>
         <div>
-            ${date}
+            ${date.toDateString()}
         </div>
         <div>
            ${content}
