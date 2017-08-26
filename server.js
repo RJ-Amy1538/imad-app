edcf36a04ad5e377b6ab5589ee8ed7aebc429c13
RJ-Amy1538 <31036053+RJ-Amy1538@users.noreply.github.com>
2017-08-26 15:42:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,6 +136,7 @@ app.get('/articles/:articleName', function (req, res) {
    //articleName==article-one
    //article[articleName]== content object of article one
    
+   // SELECT * FROM articles WHERE title = 'article-one'
   pool.query("SELECT * FROM articles WHERE title = ' "  + req.params.articleName + " ' ", function (err, result){
      if(err) {
          res.status(500).send(err.toString());
