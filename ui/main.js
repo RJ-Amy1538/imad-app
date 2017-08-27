4fed2908c16809e095d588484b4e215e879834ca
RJ-Amy1538 <31036053+RJ-Amy1538@users.noreply.github.com>
2017-08-27 10:41:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,5 @@
-//counter code
 
-var button = document.getElementById('counter');
-button.onclick = function () {
-  // create a request object
-  var request = new XMLHttpRequest();
-  
-  
-  //capture the response and store it in a variable
-  request.onreadystatechange = function () {
-    if(request.readyState === XMLHttpRequest.DONE) {
-        //take some action
-        if(request.status === 200) {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
-     //not done yet
-  };
-  //make a request
-  request.open('GET', "http://rockstar1538.imad.hasura-app.io/counter", true);
-  request.send(null);
-};
-
-//submit name
+//submit username/password to login
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
@@ -38,22 +14,22 @@ submit.onclick = function () {
         //take some action
         if(request.status === 200) {
          //capture a list of names and render it as a list
-  var names = request.responseText;
-  names = JSON.parse(names);
-  var list = '';
-  for(var i=0;i<names.length;i++) {
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
-        }
-    }
+         console.log('user logged in');
+         alert('Logged in successfully');
+        } else if (request.status === 403) {
+            alert('Username/password is incorrect');
+       } else if(request.status === 500)  {
+           alert('Something went wrong');
+       }
+    }   
      //not done yet
   };
   //make a request
-  var nameInput = document.getElementById('name');
-  var name = nameInput.value;
-  request.open('GET', "http://rockstar1538.imad.hasura-app.io/submit-name?name=" + name, true);
-  request.send(null);
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST', "http://rockstar1538.imad.hasura-app.io/login", true);
+  request.send(JSON.stringify({username: username, password:password}));
   
 };
