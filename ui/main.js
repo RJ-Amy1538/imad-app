8f5ba4c8c0bcbb852e77fcb0ec5bb6a8ea5a496e
RJ-Amy1538 <31036053+RJ-Amy1538@users.noreply.github.com>
2017-08-27 10:43:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,6 +30,7 @@ submit.onclick = function () {
   console.log(username);
   console.log(password);
   request.open('POST', "http://rockstar1538.imad.hasura-app.io/login", true);
+  request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({username: username, password:password}));
   
 };
