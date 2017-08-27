5b3ee5cf530a5371249a13413799f24fe94a59e1
RJ-Amy1538 <31036053+RJ-Amy1538@users.noreply.github.com>
2017-08-27 09:23:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
+
 
 var config = {
      user:  'rockstar1538',
@@ -97,6 +99,19 @@ return htmlTemplate;
 
      
  }
+ 
+ function hash (input, salt){
+   //How do we create a Hash?
+   var hashed = crypto.pbkdf25ync(input, salt, 10000, 512, 'sha512');
+   return hashed.toString('hex');
+     }
+ 
+ 
+ app.get('/hash/:input', function(req, res){
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
+     
+ });
 
     var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
