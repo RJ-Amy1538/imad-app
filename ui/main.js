0a4e40c1aa73489d4e253c9c3fb949f951d21477
RJ-Amy1538 <31036053+RJ-Amy1538@users.noreply.github.com>
2017-08-27 10:01:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,7 +53,7 @@ submit.onclick = function () {
   //make a request
   var nameInput = document.getElementById('name');
   var name = nameInput.value;
-  request.open('POST', "http://rockstar1538.imad.hasura-app.io/submit-name?name=" + name, true);
+  request.open('GET', "http://rockstar1538.imad.hasura-app.io/submit-name?name=" + name, true);
   request.send(null);
   
 };
