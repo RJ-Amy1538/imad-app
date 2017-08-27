bc58c177454f970622ef79c6a4b261a941a76560
RJ-Amy1538 <31036053+RJ-Amy1538@users.noreply.github.com>
2017-08-27 15:31:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -249,6 +249,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/article.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article.js'));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
